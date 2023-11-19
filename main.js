6ea40a27556df8e708f7ecde925e0eb0406f8972
YukihisaHida <yukihisa714@gmail.com>
2023-11-19 20:10:35 +0900

    分割代入を覚えた

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Line, Point, Vector, cos, getIntersectionOfLineAndPlane, getPlaneFromVectorAndPoint, sin } from "./math.js";
+
 const CAN_W = 250;
 const CAN_H = 250;
 
@@ -17,162 +19,12 @@ document.onkeyup = (e) => {
     key[e.key] = false;
 }
 
-function degreesToRadians(degree) {
-    return degree * Math.PI / 180;
-}
-
-function sin(degree) {
-    return Math.sin(degreesToRadians(degree));
-}
-function cos(degree) {
-    return Math.cos(degreesToRadians(degree));
-}
 
 function drawCircle(x, y, r) {
     con.beginPath();
     con.arc(x, y, r, 0, 2 * Math.PI);
     con.fillStyle = "#000";
     con.fill();
-}
-
-/**
- * 
- * @param {Line} line 
- * @param {Plane} plane 
- */
-function getIntersectionOfLineAndPlane(line, plane) {
-    /*
-    x = x0 + tl
-    y = y0 + tm
-    z = z0 + tn
-
-    ax + by + cz + d = 0
-
-    a(x0 + tl) + b(y0 + tm) + c(z0 + tn) + d = 0
-    t(al + bm + cn) + ax0 + by0 + cz0 + d = 0
-    t = -(ax0 + by0 + cz0 + d) / (al + bm + cn)
-    */
-
-    const a = plane.a;
-    const b = plane.b;
-    const c = plane.c;
-    const d = plane.d;
-
-    const x0 = line.x0;
-    const y0 = line.y0;
-    const z0 = line.z0;
-
-    const l = line.l;
-    const m = line.m;
-    const n = line.n;
-
-    const t = -(a * x0 + b * y0 + c * z0 + d) / (a * l + b * m + c * n);
-
-    const x = x0 + t * l;
-    const y = y0 + t * m;
-    const z = z0 + t * n;
-
-    return new Point(x, y, z);
-}
-
-
-
-class Point {
-    /**
-     * コンストラクタ
-     * @param {number} x 座標
-     * @param {number} y 座標
-     * @param {number} z 座標
-     */
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-}
-
-class Vector {
-    /**
-     * コンストラクタ
-     * @param {number} x 要素
-     * @param {number} y 要素
-     * @param {number} z 要素
-     */
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-
-        this.length = this.getLength();
-    }
-
-    /**
-     * 長さを取得するメソッド
-     * @returns {number} 長さ
-     */
-    getLength() {
-        return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
-    }
-
-    /**
-     * 長さを変えるメソッド
-     * @param {number} newLength 新しい長さ
-     */
-    changeLength(newLength) {
-        const rate = newLength / this.length;
-        this.length = newLength;
-        this.x *= rate;
-        this.y *= rate;
-        this.z *= rate;
-    }
-
-    /**
-     * 内積を求めるメソッド
-     * @param {Vector} vector2 もう一つのベクトル
-     * @returns {number} 内積
-     */
-    getInnerProduct(vector2) {
-        return this.x * vector2.x + this.y * vector2.y + this.z * vector2.z;
-    }
-}
-
-
-class Line {
-    /**
-     * 直線の方程式
-     * P(x0, y0, z0)を通り、
-     * V(l, m, n)に平行
-     * x = x0 + tl
-     * y = y0 + tm
-     * z = z0 + tn
-     * @param {Point} point 通る点
-     * @param {Vector} vector 向きベクトル
-     */
-    constructor(point, vector) {
-        this.x0 = point.x;
-        this.y0 = point.y;
-        this.z0 = point.z;
-        this.l = vector.x;
-        this.m = vector.y;
-        this.n = vector.z;
-    }
-}
-
-class Plane {
-    /**
-     * 平面の方程式
-     * ax + by + cz + d = 0
-     * @param {number} a 
-     * @param {number} b 
-     * @param {number} c 
-     * @param {number} d 
-     */
-    constructor(a, b, c, d) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-    }
 }
 
 
@@ -202,16 +54,7 @@ class Camera {
     }
 
     updatePlane() {
-        const x0 = this.pos.x + this.normalVector.x;
-        const y0 = this.pos.y + this.normalVector.y;
-        const z0 = this.pos.z + this.normalVector.z;
-
-        const a = this.normalVector.x;
-        const b = this.normalVector.y;
-        const c = this.normalVector.z;
-        const d = - (a * x0 + b * y0 + c * z0);
-
-        this.plane = new Plane(a, b, c, d);
+        this.plane = getPlaneFromVectorAndPoint(this.normalVector, this.pos);
     }
 
     /**
@@ -252,24 +95,23 @@ class Camera {
         Y = ycosθ - zsinθ
         Z = ysinθ + zcosθ
         */
-        let x = vectorFromCamPos.x;
-        let y = vectorFromCamPos.y;
-        let z = vectorFromCamPos.z;
+
+        const { x, y, z } = vectorFromCamPos;
         // console.log(x, y);
 
-        let x1 = cos(this.rz) * x - sin(this.rz) * y;
-        let y1 = sin(this.rz) * x + cos(this.rz) * y;
-        let z1 = z;
+        const x1 = cos(this.rz) * x - sin(this.rz) * y;
+        const y1 = sin(this.rz) * x + cos(this.rz) * y;
+        const z1 = z;
         // console.log(x1, y1);
 
-        let x2 = x1;
-        let y2 = cos(-this.rx) * y1 - sin(-this.rx) * z1;
-        let z2 = sin(-this.rx) * y1 + cos(-this.rx) * z1;
+        const x2 = x1;
+        const y2 = cos(-this.rx) * y1 - sin(-this.rx) * z1;
+        const z2 = sin(-this.rx) * y1 + cos(-this.rx) * z1;
         // console.log(x2, y2);
 
-        let x3 = x2 + this.pos.x;
-        let y3 = y2 + this.pos.y;
-        let z3 = z2 + this.pos.z;
+        const x3 = x2 + this.pos.x;
+        const y3 = y2 + this.pos.y;
+        const z3 = z2 + this.pos.z;
         // console.log(x3, y3);
 
         return new Point(x3, y3, z3);
@@ -336,7 +178,7 @@ class Camera {
 
 
 const pointsList = [
-    // new Point(0, 3, 0),
+    new Point(0, 3, 0),
     new Point(1, 2, 1),
     new Point(1, 2, -1),
     new Point(-1, 2, -1),
---
+++ math.js
@@ -0,0 +1,161 @@
+function degreesToRadians(degree) {
+    return degree * Math.PI / 180;
+}
+
+export function sin(degree) {
+    return Math.sin(degreesToRadians(degree));
+}
+export function cos(degree) {
+    return Math.cos(degreesToRadians(degree));
+}
+
+
+export class Point {
+    /**
+     * コンストラクタ
+     * @param {number} x 座標
+     * @param {number} y 座標
+     * @param {number} z 座標
+     */
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+export class Vector {
+    /**
+     * コンストラクタ
+     * @param {number} x 要素
+     * @param {number} y 要素
+     * @param {number} z 要素
+     */
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.length = this.getLength();
+    }
+
+    /**
+     * 長さを取得するメソッド
+     * @returns {number} 長さ
+     */
+    getLength() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
+    }
+
+    /**
+     * 長さを変えるメソッド
+     * @param {number} newLength 新しい長さ
+     */
+    changeLength(newLength) {
+        const rate = newLength / this.length;
+        this.length = newLength;
+        this.x *= rate;
+        this.y *= rate;
+        this.z *= rate;
+    }
+
+    /**
+     * 内積を求めるメソッド
+     * @param {Vector} vector2 もう一つのベクトル
+     * @returns {number} 内積
+     */
+    getInnerProduct(vector2) {
+        return this.x * vector2.x + this.y * vector2.y + this.z * vector2.z;
+    }
+}
+
+export class Line {
+    /**
+     * 直線の方程式
+     * P(x0, y0, z0)を通り、
+     * V(l, m, n)に平行
+     * x = x0 + tl
+     * y = y0 + tm
+     * z = z0 + tn
+     * @param {Point} point 通る点
+     * @param {Vector} vector 向きベクトル
+     */
+    constructor(point, vector) {
+        this.x0 = point.x;
+        this.y0 = point.y;
+        this.z0 = point.z;
+
+        this.l = vector.x;
+        this.m = vector.y;
+        this.n = vector.z;
+    }
+}
+
+export class Plane {
+    /**
+     * 平面の方程式
+     * ax + by + cz + d = 0
+     * @param {number} a 
+     * @param {number} b 
+     * @param {number} c 
+     * @param {number} d 
+     */
+    constructor(a, b, c, d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+}
+
+/**
+ * 法線ベクトルとそのベクトルを通る一点から平面のクラスを取得する関数
+ * @param {Vector} normalVector 法線ベクトル
+ * @param {Point} point ポイント
+ */
+export function getPlaneFromVectorAndPoint(normalVector, point) {
+    const { x: a, y: b, z: c } = normalVector;
+
+    const x0 = point.x + a;
+    const y0 = point.y + b;
+    const z0 = point.z + c;
+
+    const d = - (a * x0 + b * y0 + c * z0);
+
+    return new Plane(a, b, c, d);
+}
+
+
+/**
+ * 直線と平面の交点を求める関数
+ * @param {Line} line 直線
+ * @param {Plane} plane 平面
+ */
+export function getIntersectionOfLineAndPlane(line, plane) {
+    /*
+    x = x0 + tl
+    y = y0 + tm
+    z = z0 + tn
+
+    ax + by + cz + d = 0
+
+    a(x0 + tl) + b(y0 + tm) + c(z0 + tn) + d = 0
+    t(al + bm + cn) + ax0 + by0 + cz0 + d = 0
+    t = -(ax0 + by0 + cz0 + d) / (al + bm + cn)
+    */
+
+    const { a, b, c, d } = plane;
+
+    const { x0, y0, z0 } = line;
+
+    const { l, m, n } = line;
+
+    const t = -(a * x0 + b * y0 + c * z0 + d) / (a * l + b * m + c * n);
+
+    const x = x0 + t * l;
+    const y = y0 + t * m;
+    const z = z0 + t * n;
+
+    return new Point(x, y, z);
+}
+
