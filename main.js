92ada942210c30beca36e6ece95f477c82f0303f
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-11-25 01:11:01 +0900

    軽量化とcanvas2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { Line, Point, Vector, cos, getIntersectionFromLineAndPlane, getPlaneFromVectorAndPoint, sin } from "./math.js";
 import { Edge, Vertex } from "./shape.js";
 
-const CAMERA_W = 3;
-const CAMERA_H = 3;
+const CAMERA_W = 3.2;
+const CAMERA_H = 1.8;
 
-const expandingRatio = 80;
+const expandingRatio = 256;
 
 const CAN_W = CAMERA_W * expandingRatio;
 const CAN_H = CAMERA_H * expandingRatio;
@@ -16,6 +16,9 @@ can.style.background = "#888";
 
 const con = can.getContext("2d");
 
+const can2 = document.getElementById("canvas2");
+const con2 = can2.getContext("2d");
+
 const key = {};
 document.onkeydown = e => {
     key[e.key] = true;
@@ -26,21 +29,21 @@ document.onkeyup = e => {
 }
 
 
-function drawCircle(x, y, r) {
-    con.beginPath();
-    con.arc(x, y, r, 0, 2 * Math.PI);
-    con.fillStyle = "#000";
-    con.fill();
+function drawCircle(ctx, x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = "#000";
+    ctx.fill();
 }
 
-function drawLine(x1, y1, x2, y2) {
-    con.beginPath();
-    con.lineTo(x1, y1);
-    con.lineTo(x2, y2);
-    con.closePath();
-    con.lineWIdth = 1;
-    con.strokeStyle = "#000";
-    con.stroke();
+function drawLine(ctx, x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.lineTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.closePath();
+    ctx.lineWIdth = 1;
+    ctx.strokeStyle = "#000";
+    ctx.stroke();
 }
 
 
@@ -74,9 +77,17 @@ class Camera {
         this.normalVector = new Vector(x, y, z);
     }
 
+    updateFocus() {
+        this.focus = new Point(
+            this.pos.x - this.normalVector.x,
+            this.pos.y - this.normalVector.y,
+            this.pos.z - this.normalVector.z,
+        );
+    }
+
 
     updatePlane() {
-        this.plane = getPlaneFromVectorAndPoint(this.normalVector, this.pos);
+        this.plane = getPlaneFromVectorAndPoint(this.normalVector, this.focus);
     }
 
 
@@ -92,8 +103,8 @@ class Camera {
      * @returns {Vertex} カメラ平面上の点
      */
     getProjectedVertex(vertex) {
-        const rayVector = new Vector(this.pos.x - vertex.x, this.pos.y - vertex.y, this.pos.z - vertex.z);
-        const ray = new Line(this.pos, rayVector);
+        const rayVector = new Vector(this.focus.x - vertex.x, this.focus.y - vertex.y, this.focus.z - vertex.z);
+        const ray = new Line(this.focus, rayVector);
         const intersection = getIntersectionFromLineAndPlane(ray, this.plane);
 
         return new Vertex(intersection.x, intersection.y, intersection.z, vertex.i);
@@ -118,13 +129,18 @@ class Camera {
 
         const { x, y, z } = vectorFromCamPos;
 
-        const x1 = cos(this.rz) * x - sin(this.rz) * y;
-        const y1 = sin(this.rz) * x + cos(this.rz) * y;
+        const sinRZ = sin(this.rz);
+        const cosRZ = cos(this.rz);
+        const sinRX = sin(-this.rx);
+        const cosRX = cos(-this.rx);
+
+        const x1 = cosRZ * x - sinRZ * y;
+        const y1 = sinRZ * x + cosRZ * y;
         const z1 = z;
 
         const x2 = x1;
-        const y2 = cos(-this.rx) * y1 - sin(-this.rx) * z1;
-        const z2 = sin(-this.rx) * y1 + cos(-this.rx) * z1;
+        const y2 = cosRX * y1 - sinRX * z1;
+        const z2 = sinRX * y1 + cosRX * z1;
 
         const x3 = x2 + this.pos.x;
         const y3 = y2 + this.pos.y;
@@ -148,7 +164,7 @@ class Camera {
 
                 const dx = (toDrawVertex.x - this.pos.x) * expandingRatio + CAN_W / 2;
                 const dy = (toDrawVertex.z - this.pos.z) * -expandingRatio + CAN_H / 2;
-                drawCircle(dx, dy, 5);
+                drawCircle(con, dx | 0, dy | 0, 2.5);
                 con.fillStyle = "#fff";
                 con.fillText(toDrawVertex.i, dx, dy);
             }
@@ -168,7 +184,31 @@ class Camera {
             const dx2 = (toDrawVertex2.x - this.pos.x) * expandingRatio + CAN_W / 2;
             const dy2 = (toDrawVertex2.z - this.pos.z) * -expandingRatio + CAN_H / 2;
 
-            drawLine(dx1, dy1, dx2, dy2);
+            drawLine(con, dx1 | 0, dy1 | 0, dx2 | 0, dy2 | 0);
+        }
+
+
+        const pdx = this.pos.x * 10 + 50;
+        const pdy = this.pos.y * -10 + 50;
+        drawCircle(con2, pdx, pdy, 3);
+
+        const fdx = this.focus.x * 10 + 50;
+        const fdy = this.focus.y * -10 + 50;
+        drawCircle(con2, fdx, fdy, 2)
+        // drawLine(con2, this.pos)
+
+        for (const vertex of vertexesList) {
+            const dx = vertex.x * 10 + 50;
+            const dy = vertex.y * -10 + 50;
+            drawCircle(con2, dx, dy, 2.5);
+        }
+
+        for (const edge of edges) {
+            const dx1 = edge.vertex1.x * 10 + 50;
+            const dy1 = edge.vertex1.y * -10 + 50;
+            const dx2 = edge.vertex2.x * 10 + 50;
+            const dy2 = edge.vertex2.y * -10 + 50;
+            drawLine(con2, dx1, dy1, dx2, dy2);
         }
     }
 
@@ -206,6 +246,7 @@ class Camera {
     update() {
         this.move();
         this.updateNormalVector();
+        this.updateFocus();
         this.updatePlane();
         this.importShapes();
         this.draw();
@@ -227,6 +268,14 @@ const vertexesList = [
 for (let i = 0; i < vertexesList.length; i++) {
     vertexesList[i].i = i;
 }
+
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         for (let k = 0; k < 10; k++) {
+//             vertexesList.push(new Vertex(i, j, k, i + j + k));
+//         }
+//     }
+// }
 
 
 const edgeIndexesList = [
@@ -258,9 +307,18 @@ const camera = new Camera(new Point(0, -1, 0), 0, 0, 3, 3, 3);
 console.log(camera);
 
 function mainLoop() {
+    const st = performance.now();
+
     con.clearRect(0, 0, CAN_W, CAN_H);
+    con2.clearRect(0, 0, 100, 100);
 
     camera.update();
+
+    const et = performance.now();
+    con.fillStyle = "#fff";
+    con.fillText(`${((et - st) * 100 | 0) / 100}ms`, 10, 10);
 }
+
+// mainLoop();
 
 setInterval(mainLoop, 1000 / 60);
